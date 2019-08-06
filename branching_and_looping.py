68a7fbc431b30e3b1fd31eca2089b99a5571bfd4
Aniruddha Agarwal <aniruddha.agarwal18@gmail.com>
2019-08-06 09:10:27 +0530

    Drill Branching and Looping

--- branching_and_looping.py
+++ branching_and_looping.py
@@ -1,10 +1,26 @@
 def integers_from_start_to_end_using_range(start, end, step):
+    l = []
+    for i in range(start, end, step):
+        l.append(i)
     """return a list"""
+    return l
     pass
 
 
 def integers_from_start_to_end_using_while(start, end, step):
     """return a list"""
+    if step > 0:
+        l = []
+        while (start < end):
+            l.append(start)
+            start += step
+        return l
+    if step < 0:
+        l = []
+        while (start > end):
+            l.append(start)
+            start += step
+        return l
     pass
 
 
@@ -12,4 +28,16 @@ def two_digit_primes():
     """
     Return a list of all two-digit-primes
     """
+    l = []
+    for num in range(10,100):
+  
+       if num > 1:
+            for i in range(2,num):
+               if (num % i) == 0:
+                   break
+            else:
+                l.append(num)
+    
+    return (l)
     pass
+
