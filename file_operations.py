54db229a95f3d72113e05b575e81aeb46ad078a4
Aniruddha Agarwal <aniruddha.agarwal18@gmail.com>
2019-08-06 12:13:36 +0530

    Drill File Operation

--- file_operations.py
+++ file_operations.py
@@ -1,3 +1,4 @@
+
 """
 Implement the below file operations.
 Close the file after each operation.
@@ -5,14 +6,23 @@ Close the file after each operation.
 
 
 def read_file(path):
+    with open(path, 'r') as f:
+        s = f.read()
+    return s
+    
     pass
 
 
 def write_to_file(path, s):
+    with open(path, 'w') as f:
+        f.write(s)
+    
     pass
 
 
 def append_to_file(path, s):
+    with open(path, 'a') as f:
+        f.write(s)
     pass
 
 
@@ -26,4 +36,14 @@ def numbers_and_squares(n, file_path):
     2,4
     3,9
     """
+
+    with open(file_path, 'w') as csv_file:
+        
+        for i in range(1, n+1):
+            st1 = str(i) + ',' + str(i ** 2) + '\n'
+            csv_file.write(st1)
+
+           
+        
+
     pass
