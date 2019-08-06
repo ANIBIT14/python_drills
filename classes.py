825befa3da72c38b5b464460868684f0f619f1e9
Aniruddha Agarwal <aniruddha.agarwal18@gmail.com>
2019-08-06 11:17:55 +0530

    Drill Classes

--- classes.py
+++ classes.py
@@ -10,4 +10,12 @@ returns the distance between the two points.
 
 
 class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def distance(self, p):
+        return ((((self.x - p.x) ** 2) + ((self.y - p.y) ** 2)) ** 0.5)
+
+    
     pass
