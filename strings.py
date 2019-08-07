b9b63215aa818004bbc3298d1291930ebcb2146f
Aniruddha Agarwal <aniruddha.agarwal18@gmail.com>
2019-08-07 12:00:56 +0530

    Drill Strings

--- strings.py
+++ strings.py
@@ -1,17 +1,11 @@
 def last_3_characters(x):
-    if len(x) < 3:
-        return x
-    else:
         return x[-3:]
-    pass
+    
 
 
 def first_10_characters(x):
-    if len(x) < 10:
-        return x
-    else:
         return x[:10]
-    pass
+   
 
 
 def chars_4_through_10(x):
@@ -19,24 +13,23 @@ def chars_4_through_10(x):
         return ''
     else:
         return x[4:11]
-    pass
+    
 
 
 def str_length(x):
     return len(x)
-    pass
+    
 
 
 def words(x):
     return x.split()
-    pass
-
+    
 
 def capitalize(x):
     return x.capitalize()
-    pass
+    
 
 
 def to_uppercase(x):
     return x.upper()
-    pass
+    
