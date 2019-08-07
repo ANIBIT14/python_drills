098767f95cc2e038b80b3b7af948867e867f76be
Aniruddha Agarwal <aniruddha.agarwal18@gmail.com>
2019-08-07 12:03:03 +0530

    Drill Lists

--- lists.py
+++ lists.py
@@ -3,44 +3,44 @@ import copy
 
 def sum_items_in_list(x):
     return sum(x)
-    pass
+    
 
 
 def list_length(x):
     return len(x)
-    pass
+    
 
 
 def last_three_items(x):
     return x[-3:]
-    pass
+    
 
 
 def first_three_items(x):
     return x[0:3]
-    pass
+    
 
 
 def sort_list(x):
     return sorted(x)
-    pass
+    
 
 
 def append_item(x, item):
     x.append(item)
     return x
-    pass
+    
 
 
 def remove_last_item(x):
     x.pop()
     return x
-    pass
+    
 
 
 def count_occurrences(x, item):
     return x.count(item)
-    pass
+    
 
 
 def is_item_present_in_list(x, item):
@@ -48,7 +48,7 @@ def is_item_present_in_list(x, item):
         return True
     else:
         return False
-    pass
+    
 
 
 def append_all_items_of_y_to_x(x, y):
@@ -57,7 +57,7 @@ def append_all_items_of_y_to_x(x, y):
     """
     x.extend(y)
     return x
-    pass
+    
 
 
 def list_copy(x):
@@ -65,4 +65,4 @@ def list_copy(x):
     Create a shallow copy of x
     """
     return copy.copy(x)
-    pass
+    
